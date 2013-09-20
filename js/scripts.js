90e2ef97d2d434bb4921c52ba68d7b85695fa291
Alex Semeniuc <as@inlead.dk>
2013-09-20 11:46:31 +0300

    Case 3342 - Fixed placeholder attribute in search form.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 (function($) {
   $(function() {
-    // ...
+    $('.form-item-search-block-form > .ui-autocomplete-input').attr('placeholder', Drupal.t("Search materials, issues and more."));
   });
 })(jQuery);
